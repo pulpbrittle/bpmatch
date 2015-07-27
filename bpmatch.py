56a60d28941622138d85218e48fd9e36d5ac628d
Alireza Bashiri <pulpbrittle@gmail.com>
2015-07-27 21:44:07 +0430

    Add comments for better understanding

--- bpmatch.py
+++ bpmatch.py
@@ -6,7 +6,17 @@ import os
 
 
 def find_py(path):
-    """used to find all python files (recursively) in the directory"""
+    """Used to find all python files (recursively) in the directory
+    
+    Args:
+        path (str): a path to search .py files in the module
+
+    Example:
+        >>> find_py('/home/pulpbrittle')
+        ['/home/pulpbrittle/foo.py',
+         '/home/pulpbrittle/bar.py']
+    
+    """
     pyfiles = []
     for root, dirnames, filenames in os.walk(path):
         for filename in fnmatch.filter(filenames, '*.py'):
@@ -15,7 +25,19 @@ def find_py(path):
 
 
 def find_bp(lst, verbose=None):
-    """used to find the defined blueprints in a module"""
+    """Used to find the defined blueprints in a module
+
+    Args:
+        lst (list): a list of .py files in the module
+        verbose (bool): prints verbose information
+
+    Example:
+        >>> find_bp(['/home/pulpbrittle/foo.py',
+                     '/home/pulpbrittle/bar.py'])
+        ["mod = Blueprint('orders', __name__, url_prefix='/orders')",
+         "mod = Blueprint('shipping', __name__, url_prefix='/shipping')"]
+
+    """
     bp = []
     p = re.compile('[a-zA-Z_0-9]+\s\=\sBlueprint\((.*)\)')
 
@@ -29,11 +51,21 @@ def find_bp(lst, verbose=None):
                         print '{:03d}->{} └── {}'.format(number, line, path)
 
     def split_bp():
-        """used to split all the parameters of the blueprint"""
+        """Used to split all the parameters of the blueprint
+        
+
+        Example:
+            >>> raw_bp = ["mod = Blueprint('orders', __name__)",
+                          "mod = Blueprint('shipping', __name__)"]
+            >>> split_bp(raw_bp)
+            ['orders', 'shipping']
+
+        """
         params = []
         p = re.compile(ur'\(\s*([^)]+?)\s*\)')
         for _ in bp:
             for match in p.findall(_):
                 params.append(match.split(', ')[0])
         return params
+
     return split_bp()
